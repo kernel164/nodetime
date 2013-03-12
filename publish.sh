10e985b731824d87950e82c4744809f1a1d01817
Dmitri Melikyan <dmitri@rapidlogix.com>
2013-03-12 01:40:43 +0100

    metric creation fix

--- publish.sh
+++ publish.sh
@@ -1,12 +1,11 @@
 TAG=$1
 MSG=$2
 
-echo $TAG
-echo $MSG
+echo "pushing and publishing $TAG ($MSG)"
 
 git add .
 git commit -am "$MSG"
-#git tag -a "$TAG" -m "$MSG"
-#git push origin master
-#git push origin $TAG
-#npm publish
+git tag -a "$TAG" -m "$MSG"
+git push origin master
+git push origin $TAG
+npm publish
