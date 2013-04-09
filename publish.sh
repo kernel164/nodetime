c673d1f6e39e9496c54bc7a207d46098f9859f34
Dmitri Melikyan <dmitri@rapidlogix.com>
2013-04-09 16:14:04 +0200

    precompiled versions for 0.10.2

--- publish.sh
+++ publish.sh
@@ -3,8 +3,8 @@ MSG=$2
 
 echo "pushing and publishing $TAG ($MSG)"
 
-#git add .
-#git commit -am "$MSG"
+git add .
+git commit -am "$MSG"
 git tag -a "$TAG" -m "$MSG"
 git push origin master
 git push origin $TAG
